c59db3692369c2eb0a431a45dbb6e812e145b9bc
Paulko335 <pauletiennecourville@hotmail.com>
2020-03-16 16:27:36 -0400

    Added exercises 6.16 - 19

--- Source_Code/Chap_06/Creativity/C-6.16/C-6.16.py
+++ Source_Code/Chap_06/Creativity/C-6.16/C-6.16.py
@@ -4,3 +4,8 @@
 # constructor (that defaults to None). If push is called when the stack is at
 # full capacity, throw a Full exception (defined similarly to Empty).
 
+from array_stack_6_16 import ArrayStack
+
+stack = ArrayStack(maxlen=1)
+stack.push(0)
+stack.push(1)
